b149822752c7fabeec12b1a6670a1404bce2686b
Sergii <deviceekreative@ek-mm-029.local>
2018-09-25 11:53:02 +0300

    device advertising setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,23 @@
 // #include "hap.h"
 #include <WiFi.h>
 #include "mdns.h"
+#include "esp_wifi.h"
+//this include is in gitignore, store security credentials there
+#include "access/access.h"
 
-#define WIFI_SSID "Historic"
-#define WIFI_PASS "Cedk49bV48"
 #define PORT 14000
+
+//SERVICE
+#define HAP_SERVICE "_hap"
+#define HAP_PROTO "_tcp"
+#define DEVICE_NAME "RGB Light"
 
 WiFiServer server(80);
 
 void wifi_setup() {
-    Serial.println("Connecting to network");
 
     delay(1000);
+    Serial.println("Connecting to network");
     
     WiFi.begin(WIFI_SSID, WIFI_PASS);
     while (WiFi.status() != WL_CONNECTED) {
@@ -33,7 +39,6 @@ void wifi_setup() {
 
 void mdns_setup() {
 
-    Serial.println("start mdns_init");
     esp_err_t status = mdns_init();
     delay(2000);
     if (status) {
@@ -42,7 +47,6 @@ void mdns_setup() {
         Serial.println("mdns_init ok...");
     }
 
-    Serial.println("start mdns_hostname_set");
     status = mdns_hostname_set("lights.local");
     delay(2000);
     if (status) {
@@ -51,10 +55,58 @@ void mdns_setup() {
         Serial.println("mdns_hostname_set ok...");
     }
     
+    status = mdns_instance_name_set(DEVICE_NAME);
+    if (status) {
+        Serial.println("Error mdns_instance_name_set");
+    } else {
+        Serial.println("mdns_instance_name_set ok...");
+    }
+
+
+    status = mdns_service_add(DEVICE_NAME, HAP_SERVICE, HAP_PROTO, 14000, NULL, 0);
+    if (status) {
+        Serial.println("Error mdns_service_add");
+    } else {
+        Serial.println("mdns_service_add ok...");
+    }
+
+    uint8_t mac[6];
+    esp_wifi_get_mac(ESP_IF_WIFI_STA, mac);
+    char accessory_id[32] = {0,};
+    sprintf(accessory_id, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    Serial.print("Accessory ID: ");
+    Serial.print(accessory_id);
+    Serial.println("");
+
+    char pairState[4];
+    char category[4];
+    memset(pairState, 0, sizeof(pairState));
+    sprintf(pairState, "%d", 1);
+    memset(category, 0, sizeof(category));
+    sprintf(category, "%d", 1);
+    
+    mdns_txt_item_t hap_service_txt[8] = {
+        {(char*)"c#", (char*)"1.0"},
+        {(char*)"ff", (char*)"0"},
+        {(char*)"pv", (char*)"1.0"},
+        {(char*)"id", (char*)accessory_id},
+        {(char*)"md", (char*)DEVICE_NAME},
+        {(char*)"s#", (char*)"1"},
+        {(char*)"sf", (char*)pairState},
+        {(char*)"ci", (char*)category},
+    };
+    
+    status = mdns_service_txt_set(HAP_SERVICE, HAP_PROTO, hap_service_txt, 8);
+    if (status) {
+        Serial.println("Error mdns_service_txt_set");
+    } else {
+        Serial.println("mdns_service_txt_set ok...");
+    }
+    
 }
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
     wifi_setup();
     mdns_setup();
 }
